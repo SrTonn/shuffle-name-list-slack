126f3b30fdfff3171acc62699ec6708b42da4c2d
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-17 19:47:48 -0300

    regra de negocio adicionada

--- src/components/Context/Provider.jsx
+++ src/components/Context/Provider.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Context from './Context'
 
 export default function Provider({ children }) {
-  const context = {};
+  const [ nameList, setNameList ] = useState('');
+  const [ inputTextArea, setInputTextArea] = useState('');
+
+  const handleClick = () => {
+    setNameList(catchNameList(inputTextArea))
+  }
+
+  const handleChange = ({ target: {value} }) => {
+    setInputTextArea(value)
+  }
+
+  const catchNameList = (input) => {
+    const reFindHour = /(<\s)?\d{1,2}\s.*ago/i
+    const reFindEmoticons = /:.*:/g
+    let lines = input.split('\n')
+    const hasHour = lines.some((line) => reFindHour.test(line));
+    lines = hasHour ? lines.filter((line) => reFindHour.test(line)) : lines;
+    const uniqueUsers = lines
+      .map((name) => name.replace(reFindHour, '')
+      .replace(reFindEmoticons, '')
+      .trim());
+
+    return shuffle([...new Set(uniqueUsers)]).join('\n')
+  }
+
+  const shuffle = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+  // Ref.: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+
+  const context = {
+    handleChange,
+    catchNameList,
+    handleClick,
+    nameList,
+  };
 
   return (
     <Context.Provider value={context}>
