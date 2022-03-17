e29a941f3bcc127ad1a566a4a6a30497599cf6ac
Wellington Rodrigues <ton.rodrigues@icloud.com>
2022-03-17 19:52:37 -0300

    pagina Home criada

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Main from '../components/Main/Main';
+
+export default function Home() {
+
+  return (
+    <>
+      <h1>Misturador de nomes do slack</h1>
+      <h3>Copie a sua lista para a área em baixo e clique em ordenar items aleatoriamente.</h3>
+      <Main />
+    </>
+  );
+}
